Mahmoudk1000/packages_apps_Settings-1

Settings: Add Device Name & CodeName Prop
Change-Id: I61fd4d77375df019da94ed7fca71da157dd0f415
Signed-off-by: ROXAANE <bajithottyadvaith99@gmail.com>

src/com/android/settings/deviceinfo/firmwareversion/AboutDeviceNamePreferenceController.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2019 ArrowOS
+ * Copyright (C) 2020 Elegant-UI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class AboutDeviceNamePreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "AboutDeviceNameCtrl";
+
+    private static final String KEY_DEVICE_NAME_PROP = "org.elegant.device";
+    private static final String KEY_DEVICE_CODENAME_PROP = "ro.product.model";
+
+    public AboutDeviceNamePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String deviceName = SystemProperties.get(KEY_DEVICE_NAME_PROP,
+                mContext.getString(R.string.unknown));
+        String deviceCodename = SystemProperties.get(KEY_DEVICE_CODENAME_PROP,
+                mContext.getString(R.string.unknown));
+	    return deviceName + " | " + deviceCodename;
+    }
+}

TenX-OS/packages_apps_Settings

Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

src/com/android/settings/deviceinfo/firmwareversion/AboutDeviceNamePreferenceController.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2019 ArrowOS
+ * Copyright (C) 2020 TenX-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class AboutDeviceNamePreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "AboutDeviceNameCtrl";
+
+    private static final String KEY_DEVICE_NAME_PROP = "org.tenx.device";
+    private static final String KEY_DEVICE_CODENAME_PROP = "ro.product.model";
+
+    public AboutDeviceNamePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String deviceName = SystemProperties.get(KEY_DEVICE_NAME_PROP,
+                mContext.getString(R.string.unknown));
+        String deviceCodename = SystemProperties.get(KEY_DEVICE_CODENAME_PROP,
+                mContext.getString(R.string.unknown));
+	    return deviceName + " | " + deviceCodename;
+    }
+}